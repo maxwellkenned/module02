878779f071798c8db65bd1e8e50143265199545c
Maxwell Kenned <kenned123@gmail.com>
2019-10-24 00:01:54 -0300

    Alteraçoes ate a aula 2/8/1 configurando Multer

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -13,5 +16,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/users', UserController.list);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
