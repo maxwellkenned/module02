adbe6936829c0440a5cd07c0cd86795fd6cc081d
Maxwell Kenned <maxwell_kenned@voxtecnologia.com.br>
2019-10-25 16:29:38 -0300

    Alterações até a aula 2/8/2 Avatar do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,8 @@
 import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
 
+import File from './File';
+
 class User extends Model {
   static init(sequelize) {
     super.init(
@@ -11,16 +13,20 @@ class User extends Model {
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
-      { sequelize },
+      { sequelize }
     );
 
-    this.addHook('beforeSave', async (user) => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
 
     return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
   }
 
   checkPassword(password) {
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,8 +18,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/users', UserController.list);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
