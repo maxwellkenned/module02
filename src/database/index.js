5f8d5266f3e35a2ae68cc21853711fc1a57464c8
Maxwell Kenned <kenned123@gmail.com>
2019-11-06 23:21:55 -0300

    Alteraçoes ate a aula 2/8/6 Validaçoes de agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import Files from '../app/models/File';
+import Appointment from '../app/models/Appointments';
 
-const models = [User, Files];
+const models = [User, Files, Appointment];
 
 class Database {
   constructor() {
